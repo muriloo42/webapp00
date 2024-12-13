3d818bae87924384f8773f87babd876c2d209012
muriloo42 <abreu.murilo20@gmail.com>
2024-12-13 09:06:34 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,6 +4,9 @@ import streamlit as st
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
 st.title("murilo brazuna sato")
 
+a = float(st.test_input("Digite valor a: "))
+st.write(a)
+
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
 st.header("Cabeçalho")
 
